e37938b4c01c72c254a7435f7b78fb3122b5c8c1
VITAL070702 <yvitalraquel@gmail.com>
2025-03-27 18:48:13 -0700

    agregado  profileCard

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,8 +1,26 @@
-function ProfileCard(props) {
-    console.log(props)
-  return (
-    <div>ProfileCard2</div>
-  )
-}
 
-export default ProfileCard
+function ProfileCard({title, handle, image}) {
+
+    return (
+       
+      <div className="card">
+        <div className="card-image">
+            <figure className="image is 1by1">
+                <img src={image} alt="" width={200} height={200}/>
+                
+            </figure>
+        </div>
+             <div className="card-content">
+            <div className="media-content">
+                <p className="title is-4">Title is {title}</p>
+                <p className="subtitle is-6">handle is {handle}</p>
+            </div>
+        </div>
+      </div>
+     
+    )
+   
+  }
+  
+  export default ProfileCard
+  
