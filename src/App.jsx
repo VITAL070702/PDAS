e32a43760e4a5170430addc0820baee9c2cab47f
VITAL070702 <yvitalraquel@gmail.com>
2025-03-27 19:39:30 -0700

    agregado App

--- src/App.jsx
+++ src/App.jsx
@@ -15,24 +15,24 @@ function App() {
             <div className="columns">
               <div className="column is-4">
                 <ProfileCard 
-                title="Siri"
+                title="SIRI"
                 handle="@siri"
-                image ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQySxmPfYLTLVKi_Q-Lqhcx2sqbPqYefYIX3DxYXG1wA1Z8cwk9L7BpSlvuHQ4YK_geISA&usqp=CAU"
+                image ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWavQKwpdAOSvp5bdk9kbE-leCCjvsv5YhRA&s"
                 />
                 
               </div>
               <div className="column is-4">
                 <ProfileCard 
-                 title="Siri"
+                 title="CORTANA"
                  handle="@vision genesis"
-                 image ="https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.wikipedia.org%2Fwiki%2FMicrosoft_Cortana&psig=AOvVaw2DOKHXNKw_YT422Glvqsz6&ust=1743215476681000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNDX0evdq4wDFQAAAAAdAAAAABAE"/>
+                 image ="https://upload.wikimedia.org/wikipedia/commons/4/4d/Microsoft_Cortana_logo.png"/>
                 
               </div>
               <div className="column is-4">
                 <ProfileCard  
-                 title="Siri"
+                 title="BIXVI"
                  handle="@BixVi"
-                 image ="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.androidpolice.com%2Fwhat-is-samsung-bixby%2F&psig=AOvVaw2j3EH5j-CVoXVollNIYxWW&ust=1743215497879000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOjN-_ndq4wDFQAAAAAdAAAAABAE"/>
+                 image ="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Bixby_logo.svg/2560px-Bixby_logo.svg.png"/>
                 
               </div>
             </div>
